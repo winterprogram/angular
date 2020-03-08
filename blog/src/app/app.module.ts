20754c14b83ed3c976ec8d9308c5a6b4c4025e2c
winterprogram <chakladar.sandeep4@gmail.com>
2020-03-09 00:13:41 +0530

    adding routing and other custom components

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { AboutComponent } from './about/about.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogViewComponent,
+    BlogEditComponent,
+    AboutComponent,
+    BlogCreateComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'about', component: AboutComponent },
+      {path:'blog/:blogId',component:BlogViewComponent},
+      {path:'create', component: BlogCreateComponent },
+      { path:'edit/:blogId', component:BlogEditComponent},
+      {path : '*****', component : NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
