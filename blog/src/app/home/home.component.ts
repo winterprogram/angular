52b5c049a826f71e792256993b273e512e37d3c5
winterprogram <chakladar.sandeep4@gmail.com>
2020-03-10 00:24:05 +0530

    adding html and loop in home page

--- blog/src/app/home/home.component.ts
+++ blog/src/app/home/home.component.ts
@@ -7,6 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public allBlogs = [
+
+    {
+      "blogId": "1",
+      "created": "2020-10-20T12:20:47.854z",
+      "tags": [],
+      "author": "Admin",
+      "category": "Comedy",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "this is the body ",
+      "description": "this is blog1 description",
+      "title": "this is blog 1"
+    },
+    {
+      "blogId": "2",
+      "created": "2020-10-20T12:20:47.854z",
+      "tags": [],
+      "author": "Admin",
+      "category": "adventure",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "this the body ",
+      "description": "this is blog1",
+      "title": "this is blog 2"
+    },
+    {
+      "blogId": "3",
+      "created": "2020-10-20T12:20:47.854z",
+      "tags": [],
+      "author": "Admin",
+      "category": "adventure",
+      "isPublished": true,
+      "views": 0,
+      "bodyHtml": "this the body ",
+      "description": "this is blog1 boogi",
+      "title": "this is blog 3"
+    }
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
